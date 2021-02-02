ba1f85e9ef9791d315c38269d09d77bd6dd541b9
Sebi <stoicescusebastian@yahoo.com>
2021-02-02 11:58:51 +0200

    First commit

---
+++ src/containers/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList.js';
+import { robots } from '../robots';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll.js'
+import './App.css';
+
+
+class App extends Component {
+    constructor(){
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (event) =>{
+        this.setState({ searchfield: event.target.value })
+
+    }
+
+    render(){
+
+        const filteredRobots = this.state.robots.filter( robot =>{
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLocaleLowerCase())
+        })
+        return (
+            <div className='tc'>
+                <h1 className="f1"> RoboFriends </h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <Scroll>
+                    <CardList robots={filteredRobots} />
+                </Scroll>
+            </div>
+        );
+
+    }
+}
+
+export default App;
